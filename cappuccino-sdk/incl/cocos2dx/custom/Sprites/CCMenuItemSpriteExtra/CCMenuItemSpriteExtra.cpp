725161b92082e25e5c62a3dcc7511856caaca455
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2021-01-11 08:55:21 -0600

    actually fix it lmfao

--- cappuccino-sdk/incl/cocos2dx/custom/Sprites/CCMenuItemSpriteExtra/CCMenuItemSpriteExtra.cpp
+++ cappuccino-sdk/incl/cocos2dx/custom/Sprites/CCMenuItemSpriteExtra/CCMenuItemSpriteExtra.cpp
@@ -1,34 +1,33 @@
 #include "CCMenuItemSpriteExtra.h"
 
-void CCMenuItemSpriteExtra::selected(){
+void CCMenuItemSpriteExtra::selected() {
 	CCMenuItemSprite::selected();
 	auto resize = cocos2d::CCScaleTo::create(0.3, m_sizeMult * m_origScale);
 	auto bounce = cocos2d::CCEaseBounceOut::create(resize);
 	this->runAction(bounce);
 }
 
-void CCMenuItemSpriteExtra::unselected(){
+void CCMenuItemSpriteExtra::unselected() {
 	CCMenuItemSprite::unselected();
 	auto resize = cocos2d::CCScaleTo::create(0.3, m_origScale);
 	auto bounce = cocos2d::CCEaseBounceOut::create(resize);
 	this->runAction(bounce);
 }
 
-void CCMenuItemSpriteExtra::activate () {
+void CCMenuItemSpriteExtra::activate() {
 	CCMenuItemSprite::activate();
 	this->stopAllActions();
-	auto reset = cocos2d::CCScaleTo::create(0.0, m_origScale);
-	this->runAction(reset);
+	this->setScale(m_origScale);
 }
 
 void CCMenuItemSpriteExtra::setScale(float scale) {
-	CCMenuItemSprite::setScale(scale);	
+	CCMenuItemSprite::setScale(scale);
 	m_origScale = scale;
 }
 
-CCMenuItemSpriteExtra* CCMenuItemSpriteExtra::create(CCNode *normalSprite, CCNode *selectedSprite, CCObject *target, cocos2d::SEL_MenuHandler selector){
+CCMenuItemSpriteExtra* CCMenuItemSpriteExtra::create(CCNode* normalSprite, CCNode* selectedSprite, CCObject* target, cocos2d::SEL_MenuHandler selector) {
 	auto spriteItem = new CCMenuItemSpriteExtra;
-	if (spriteItem && spriteItem->initWithNormalSprite(normalSprite, selectedSprite, nullptr, target, selector)) 
+	if (spriteItem && spriteItem->initWithNormalSprite(normalSprite, selectedSprite, nullptr, target, selector))
 		spriteItem->autorelease();
 	else {
 		delete spriteItem;
--- cappuccino-sdk/incl/cocos2dx/custom/Sprites/CCMenuItemSpriteExtra/CCMenuItemSpriteExtra.h
+++ cappuccino-sdk/incl/cocos2dx/custom/Sprites/CCMenuItemSpriteExtra/CCMenuItemSpriteExtra.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cocos2d.h>
 
-class CCMenuItemSpriteExtra : public cocos2d::CCMenuItemSprite{
+class CCMenuItemSpriteExtra : public cocos2d::CCMenuItemSprite {
 private:
 	float m_sizeMult = 1.25;
 	float m_origScale = 1.f;
@@ -10,6 +10,6 @@ public:
 	virtual void unselected() override;
 	virtual void activate() override;
 	virtual void setScale(float scale) override;
-	static CCMenuItemSpriteExtra* create(CCNode *normalSprite, CCNode *selectedSprite, CCObject *target, cocos2d::SEL_MenuHandler selector);
+	static CCMenuItemSpriteExtra* create(CCNode* normalSprite, CCNode* selectedSprite, CCObject* target, cocos2d::SEL_MenuHandler selector);
 	void setSizeMult(float multiplier) { m_sizeMult = multiplier; }
 };
